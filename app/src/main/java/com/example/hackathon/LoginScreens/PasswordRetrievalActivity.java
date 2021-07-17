fd8513efeecc08e8d7f1149bb744386b0ab17b7e
Darren Yee <dyee0005@student.monash.edu>
2021-07-17 16:49:47 +0800

    LoginScreens added

---
+++ app/src/main/java/com/example/hackathon/LoginScreens/PasswordRetrievalActivity.java
@@ -0,0 +1,16 @@
+package com.example.hackathon.LoginScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hackathon.R;
+
+public class PasswordRetrievalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_retrieval);
+    }
+}
---
+++ app/src/main/java/com/example/hackathon/LoginScreens/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.hackathon.LoginScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hackathon.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
