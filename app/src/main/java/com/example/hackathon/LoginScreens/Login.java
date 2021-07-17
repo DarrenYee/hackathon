7220bc97d0498dae039201bec588f5fd3a83e6ee
Darren Yee <dyee0005@student.monash.edu>
2021-07-17 17:38:35 +0800

    LoginScreens added

--- app/src/main/java/com/example/hackathon/LoginScreens/Login.java
+++ app/src/main/java/com/example/hackathon/LoginScreens/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 import com.example.hackathon.R;
 
 public class Login extends AppCompatActivity {
@@ -12,5 +13,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
