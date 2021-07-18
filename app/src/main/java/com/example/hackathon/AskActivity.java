f89f88117d90127fd867d671b8852689f5a47555
Kin <jkin0011@student.monash.edu>
2021-07-18 15:31:54 +0800

    Added AskActivity step 2

--- app/src/main/java/com/example/hackathon/AskActivity.java
+++ app/src/main/java/com/example/hackathon/AskActivity.java
@@ -3,12 +3,32 @@ package com.example.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AskActivity extends AppCompatActivity {
+
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("hackathon/ask");
+    }
+
+//    public String readInput(){
+//        EditText userInput = findViewById(R.id.userInput);
+//        return userInput.getText().toString();
+//    }
+
+    public void submit(View view){
+//        myRef.push().setValue(readInput());
+        myRef.push().setValue("yjsb");
     }
 }
